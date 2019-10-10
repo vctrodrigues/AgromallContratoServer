a4e052bf94460fc03899c856cad10239cfa904d5
Victor Vieira <victorvieira89@gmail.com>
2019-10-10 10:49:35 -0300

    initialize server

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,5 @@
 const app = require("../src/app");
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || "80";
 
 app.server.listen(port, () => {
   console.log(`Server stated on port ${port}`);
